9a60d94f38daa449615d88b426d7fd2b8da79f4f
Dan Iverson <dan@daniverson.com>
2023-09-30 05:49:24 -0500

    Create provider for OCI

--- provider.tf
+++ provider.tf
@@ -1,10 +1,17 @@
 provider "oci" {
-    auth = "InstancePrincipal"
-    region = var.region
+  # Cloud Shell Authentiation
+  # auth = "InstancePrincipal"
+  # region = var.region
+
+  # Local Shell Authentication
+  config_file_profile=var.config_file_profile
 }
 
 variable "region" {}
 variable "tenancy_id" {}
+variable "config_file_profile" {
+  default = "DEFAULT"
+}
 
 data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_id
